2e1ecde01c5fa593c77eb4a8fdd2e22eb48322d2
Vuk Antović <antovic@users.noreply.github.com>
2026-04-19 11:24:47 +0000

    #6 feat(turner): implement NAV-BE-002.1-S2 return EAST for R from NORTH

--- tests/test_nav_be_002_1.py
+++ tests/test_nav_be_002_1.py
@@ -12,3 +12,15 @@ def test_nav_be_002_1_s1_turner_returns_west_for_l_from_north():
 
     # THEN
     assert new_state == RoverState(x=0, y=0, heading=Heading.WEST)
+
+
+def test_nav_be_002_1_s2_turner_returns_east_for_r_from_north():
+    # GIVEN
+    state = RoverState(x=0, y=0, heading=Heading.NORTH)
+    turner = Turner()
+
+    # WHEN
+    new_state = turner.turn(state, "R")
+
+    # THEN
+    assert new_state == RoverState(x=0, y=0, heading=Heading.EAST)
